7bf63d1d6e0e18fca62a9b4bbcac85181f44043d
Ian Ramos <dev1an@protonmail.com>
2025-05-02 12:32:17 -0300

    Added docker deployment setup for dev/prod

---
+++ docker/production/php-fpm/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+# Initialize storage directory if empty
+# -----------------------------------------------------------
+# If the storage directory is empty, copy the initial contents
+# and set the correct permissions.
+# -----------------------------------------------------------
+if [ ! "$(ls -A /var/www/storage)" ]; then
+  echo "Initializing storage directory..."
+  cp -R /var/www/storage-init/. /var/www/storage
+  chown -R www-data:www-data /var/www/storage
+fi
+
+# Remove storage-init directory
+rm -rf /var/www/storage-init
+
+# Run Laravel migrations
+# -----------------------------------------------------------
+# Ensure the database schema is up to date.
+# -----------------------------------------------------------
+php artisan migrate --force
+
+# Clear and cache configurations
+# -----------------------------------------------------------
+# Improves performance by caching config, routes, events, view.
+# -----------------------------------------------------------
+php artisan config:cache
+php artisan route:cache
+php artisan event:cache
+php artisan view:cache
+
+php artisan queue:restart
+
+# Run the default command
+exec "$@"
